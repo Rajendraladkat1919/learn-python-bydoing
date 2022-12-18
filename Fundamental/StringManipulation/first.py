53eae4e746052432d2c3ad8889a74951ce49f9e8
RajendraLadkat <ralaonfire@example.com>
2022-12-18 14:20:55 +0530

    Initial commit

---
+++ Fundamental/StringManipulation/first.py
@@ -0,0 +1,22 @@
+#python string basic
+
+# print the string
+
+str="hello python programming"
+
+print(str)
+
+str1 = 'Welcome to python coding'
+
+print(str1)
+
+multuline = """
+
+Oh my god ! This is awesome feature.
+
+I love the way python design.
+
+Really its awesome things to exploere.
+"""
+
+print(multuline)
---
+++ README.md
@@ -0,0 +1,7 @@
+This repository is created to understand basic programming concepts of Python programming. 
+
+It contains basic programs which helps to understand basic syntax and strcture of python programming.
+
+# Repository contains below folders.
+
+1. Fundamental - Fundamental of python
